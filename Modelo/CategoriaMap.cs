da0a5cc00171833a1e426e3503774a287509ad5a
cleitonsilva185 <cleitonsilva185@gmail.com>
2016-04-08 21:50:26 -0300

    Incluido Categoria
    Atualizado Categoria

---
+++ Modelo/CategoriaMap.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using FluentNHibernate.Mapping;
+
+namespace Modelo
+{
+    public class CategoriaMap: ClassMap<Categoria>
+    {
+        public CategoriaMap()
+        {
+            Id(x => x.Id).GeneratedBy.GuidComb();
+            Map(x => x.Nome);
+            
+        }
+    }
+}
